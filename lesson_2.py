34d1a03266a286620e58c2f1d72c1e905ef405ae
Ariful Islam <arifcse209@gmail.com>
2025-11-09 15:18:19 +0600

    Add base model and user table schema definition in lesson_2.py

---
+++ lesson_2.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+"""
+CREATE TABLE IF NOT EXISTS users
+(
+    telegram_id
+    BIGINT
+    PRIMARY
+    KEY,
+    full_name
+    VARCHAR
+(
+    255
+) NOT NULL,
+    username VARCHAR
+(
+    255
+),
+    language_code VARCHAR
+(
+    255
+) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW
+(
+),
+    referrer_id BIGINT,
+    FOREIGN KEY
+(
+    referrer_id
+)
+    REFERENCES users
+(
+    telegram_id
+)
+    ON DELETE SET NULL
+    );
+
+"""
