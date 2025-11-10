85d98017c4511e07a79176dae2d67be6e934026c
Ariful Islam <arifcse209@gmail.com>
2025-11-10 10:37:04 +0600

    Refactor user addition logic to handle conflicts and improve retrieval in lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -14,26 +14,33 @@ class Repo:
         self.session = session
 
     def add_user(self, telegram_id: int, full_name: str, username: str, language_code=None):
-        stmt = insert(User).values(
-            telegram_id=telegram_id,
-            full_name=full_name,
-            username=username,
-            language_code=language_code
+        stmt = select(User).from_statement(
+            insert(User).values(
+                telegram_id=telegram_id,
+                full_name=full_name,
+                username=username,
+                language_code=language_code
+            ).returning(User).on_conflict_do_update(
+                index_elements=[User.telegram_id],
+                set=dict(full_name=full_name, username=username)
+            )
         )
-        print(stmt)
-        self.session.execute(stmt)
+        result = self.session.scalars(stmt).first()
         self.session.commit()
-    def get_user_by_id(self,telegram_id:int)-> User:
+
+    def get_user_by_id(self, telegram_id: int) -> User:
         stmt = select(User).where(User.telegram_id == telegram_id)
         result = self.session.execute(stmt)
         return result.scalars().first()
 
     def get_all_users(self):
-        stmt = select(User).where(or_(User.language_code == 'en' ,User.language_code =="uk"), User.username.ilike("%john%")).order_by(User.created_at.desc()).limit(10).having(User.telegram_id > 0)
+        stmt = select(User).where(or_(User.language_code == 'en', User.language_code == "uk"),
+                                  User.username.ilike("%john%"),
+                                  User.telegram_id > 0).order_by(User.created_at.desc()).limit(10)
         result = self.session.execute(stmt)
         return result.scalars().all()
 
-    def get_user_language(self,telegram_id:int)->str:
+    def get_user_language(self, telegram_id: int) -> str:
         stmt = select(User.language_code).where(User.telegram_id == telegram_id).order_by(User.created_at.desc())
         result = self.session.execute(stmt)
         return result.scalars().first()
